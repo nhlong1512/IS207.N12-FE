1670a8ac8366324dadf4e1331f149b0d81129098
Tr0ngTin <71576097+tr0ngtin11@users.noreply.github.com>
2022-11-03 01:02:11 +0700

    cau truc folder va them 1 so package

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: false,
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
